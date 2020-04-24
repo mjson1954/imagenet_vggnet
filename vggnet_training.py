00732fef4216e453f50bb3cd8cbf0f32ba7ccdb6
mjson1954 <60597442+mjson1954@users.noreply.github.com>
2020-04-24 18:21:39 +0900

    Add files via upload

---
+++ vggnet_training.py
@@ -0,0 +1,150 @@
+import torch
+import torchvision
+import torchvision.transforms as transforms
+
+import torch.nn as nn
+import torch.nn.functional as F
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+import json
+import os
+
+transform = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+])
+
+trainset = torchvision.datasets.ImageNet('../../data/imagenet', split='train', download=None, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True)
+
+testset = torchvision.datasets.ImageNet('../../data/imagenet', split='val', download=None, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=64, shuffle=False)
+
+idx2label = []
+cls2label = {}
+with open("./imagenet_class_index.json", "r") as read_file:
+    class_idx = json.load(read_file)
+    idx2label = [class_idx[str(k)][1] for k in range(len(class_idx))]
+    cls2label = {class_idx[str(k)][0]: class_idx[str(k)][1] for k in range(len(class_idx))}
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv = nn.Sequential(
+            #3 224 128
+            nn.Conv2d(3, 64, 3, padding=1),nn.LeakyReLU(0.2),
+            nn.Conv2d(64, 64, 3, padding=1),nn.LeakyReLU(0.2),
+            nn.MaxPool2d(2, 2),
+            #64 112 64
+            nn.Conv2d(64, 128, 3, padding=1),nn.LeakyReLU(0.2),
+            nn.Conv2d(128, 128, 3, padding=1),nn.LeakyReLU(0.2),
+            nn.MaxPool2d(2, 2),
+            #128 56 32
+            nn.Conv2d(128, 256, 3, padding=1),nn.LeakyReLU(0.2),
+            nn.Conv2d(256, 256, 3, padding=1),nn.LeakyReLU(0.2),
+            nn.Conv2d(256, 256, 3, padding=1),nn.LeakyReLU(0.2),
+            nn.MaxPool2d(2, 2),
+            #256 28 16
+            nn.Conv2d(256, 512, 3, padding=1),nn.LeakyReLU(0.2),
+            nn.Conv2d(512, 512, 3, padding=1),nn.LeakyReLU(0.2),
+            nn.Conv2d(512, 512, 3, padding=1),nn.LeakyReLU(0.2),
+            nn.MaxPool2d(2, 2),
+            #512 14 8
+            nn.Conv2d(512, 512, 3, padding=1),nn.LeakyReLU(0.2),
+            nn.Conv2d(512, 512, 3, padding=1),nn.LeakyReLU(0.2),
+            nn.Conv2d(512, 512, 3, padding=1),nn.LeakyReLU(0.2),
+            nn.MaxPool2d(2, 2)
+        )
+        #512 7 4
+
+        self.avg_pool = nn.AvgPool2d(7)
+        #512 1 1
+        self.classifier = nn.Linear(512, 1000)
+        """
+        self.fc1 = nn.Linear(512*2*2,4096)
+        self.fc2 = nn.Linear(4096,4096)
+        self.fc3 = nn.Linear(4096,10)
+        """
+
+    def forward(self, x):
+        #print(x.size())
+        features = self.conv(x)
+        #print(features.size())
+        x = self.avg_pool(features)
+        #print(avg_pool.size())
+        x = x.view(features.size(0), -1)
+        #print(flatten.size())
+        x = self.classifier(x)
+        #x = self.softmax(x)
+        return x, features
+
+
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+import torch.optim as optim
+
+save_path="/data/data1/minjoo/workspace/vgg/imagenet_vgg16_result.pth"
+
+net = Net()
+net.load_state_dict(torch.load(save_path))
+net.to(device)
+
+criterion = nn.CrossEntropyLoss().cuda()
+optimizer = optim.Adam(net.parameters(),lr=0.00001)
+
+
+for epoch in range(3):  # loop over the dataset multiple times
+    running_loss = 0.0
+    for i, data in enumerate(trainloader, 0):
+        # get the inputs
+        inputs, labels = data
+        inputs, labels = inputs.to(device), labels.to(device)
+        # zero the parameter gradients
+        optimizer.zero_grad()
+        outputs,f = net(inputs)
+        loss = criterion(outputs, labels)
+
+        loss.backward()
+        optimizer.step()
+
+        if(loss.item() > 1000):
+            print(loss.item())
+            for param in net.parameters():
+                print(param.data)
+        # print statistics
+        running_loss += loss.item()
+        if i % 50 == 49:    # print every 2000 mini-batches
+            print('[%d, %5d] loss: %.3f' %
+                  (epoch + 1, i + 1, running_loss / 50))
+            running_loss = 0.0
+    torch.save(net.state_dict(), save_path)
+
+
+print('Finished Training')
+
+class_correct = list(0. for i in range(1000))
+class_total = list(0. for i in range(1000))
+with torch.no_grad():
+    for data in testloader:
+        images, labels = data
+        images = images.cuda()
+        labels = labels.cuda()
+        outputs,_ = net(images)
+        _, predicted = torch.max(outputs, 1)
+        c = (predicted == labels).squeeze()
+        for i in range(16):
+            label = labels[i]
+            class_correct[label] += c[i].item()
+            class_total[label] += 1
+
+accuracy_sum=0
+for i in range(1000):
+    temp = 100 * class_correct[i] / class_total[i]
+    print('Accuracy of %5s : %2d %%' % (
+        idx2label[i], temp))
+    accuracy_sum+=temp
+print('Accuracy average: ', accuracy_sum/1000)
